09be195c805b2b12a43c583bd10340af82c8ed0e
xAxis47 <140302470+xAxis47@users.noreply.github.com>
2024-09-13 14:00:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+//pagetop
+$(function() {
+    var scroll = $('.pagetop');
+    var scrollShow = $('.pagetop-show');
+        $(scroll).hide();
+        $(window).scroll(function() {
+            if($(this).scrollTop() >= 300) {
+                $(scroll).fadeIn().addClass(scrollShow);
+            } else {
+                $(scroll).fadeOut().removeClass(scrollShow);
+            }
+        });
+});
+
+
+//スムーススクロール
+$(window).on('load', function() {
+	var hash = location.hash;
+	if(hash) {
+		$('body,html').scrollTop(0);
+		setTimeout(function() {
+			var target = $(hash);
+			var scroll = target.offset().top;
+			$('body,html').animate({scrollTop:scroll},500);
+		}, 100);
+	}
+});
+$(window).on('load', function() {
+    $('a[href^="#"]').click(function() {
+        var href = $(this).attr('href');
+        var target = href == '#' ? 0 : $(href).offset().top;
+            $('body,html').animate({scrollTop:target},500);
+            return false;
+    });
+});
